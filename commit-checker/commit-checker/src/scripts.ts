854923e7f81115f74519e6d34036c2c8b20b8eb2
tkmsaaaam <59013192+tkmsaaaam@users.noreply.github.com>
2025-03-06 06:59:24 +0900

    feat: get automatically

--- commit-checker/commit-checker/src/scripts.ts
+++ commit-checker/commit-checker/src/scripts.ts
@@ -1,14 +1,12 @@
-(() => {
-	chrome.runtime.onMessage.addListener((request, _, sendResponse): void => {
-		if (request.message === 'getTodaysCommit') {
-			console.log('getTodaysCommit');
-			const today = new Date().toISOString().split('T')[0];
-			const t = document.querySelector(`td[data-date="${today}"]`);
-			if (!t) {
-				sendResponse(0);
-				return;
-			}
-			sendResponse(Number(t.getAttribute('data-level')));
+chrome.runtime.onMessage.addListener((request, _, sendResponse): void => {
+	if (request.message === 'getTodaysCommit') {
+		console.log('getTodaysCommit');
+		const today = new Date().toISOString().split('T')[0];
+		const t = document.querySelector(`td[data-date="${today}"]`);
+		if (!t) {
+			sendResponse(0);
+			return;
 		}
-	});
-})();
+		sendResponse(Number(t.getAttribute('data-level')));
+	}
+});
